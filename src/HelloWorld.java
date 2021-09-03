54fab2832abd89747d4a13a998957e59583e3284
Joseph Congdon <jocongdon@yahoo.com>
2021-09-03 10:48:10 -0500

    syntax lecture

---
+++ src/HelloWorld.java
@@ -0,0 +1,50 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+//        System.out.println("Hello, Draco!");
+//        int myFavoriteNumber = 42;
+//        System.out.println(myFavoriteNumber);
+
+//        String myString =" the Answer is 42";
+//        System.out.println(myString);
+
+//        long myNumber;
+//        myNumber = 123L;
+//        System.out.println(myNumber);
+
+//        float myNumber = 3.14F;
+
+//          int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+
+        System.out.println(Integer.MAX_VALUE);
+
+        int testInteger = 2147483647;
+        System.out.println(++testInteger);
+
+    }
+
+}
