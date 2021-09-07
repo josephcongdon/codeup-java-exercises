2f86fa3e1bf007af5d889b826b67dc59e7afd77b
Joseph Congdon <jocongdon@yahoo.com>
2021-09-07 12:22:51 -0500

    1 through 3 done

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -54,18 +54,68 @@ public class ControlFlowExercises {
 //                System.out.println(i);
 //
 //            }
+        String promptUser = "";
+        do {
+            Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+            System.out.println("choose a number to go to: ");
+            int promptNumber = Integer.parseInt(scanner.nextLine());
 
-        System.out.println("choose a number to go to: ");
-        int promptNumber = Integer.parseInt(scanner.nextLine());
-        for(int i = 0; i <= promptNumber; i++) {
-            int squared = ((int) Math.pow(i, 2));
-            int cubed = ((int) Math.pow(i, 3));
-            System.out.println(i);
-            System.out.println(squared);
-            System.out.println(cubed);
-        }
+            System.out.println("here is your table");
+            System.out.printf("number ");
+            System.out.printf("squared ");
+            System.out.printf("cubed ");
+            for (int i = 1; i <= promptNumber; i++) {
+                int squared = ((int) Math.pow(i, 2));
+                int cubed = ((int) Math.pow(i, 3));
+                System.out.println(" ");
+                System.out.printf(" " + i);
+                System.out.printf(" ");
+                System.out.printf(" " + squared);
+                System.out.printf(" ");
+                System.out.printf(" " + cubed);
+            }
+            System.out.println("");
+            System.out.printf(" do you wish to continue? ");
+            promptUser = scanner.nextLine();
+        } while(promptUser.equals("no"));
+
+
+
+
+
+//        int c = 0, d = 100, e = 50;
+//        if( c == 0 && e++ < 100 )
+//        {
+//            d = 150;
+//        }
+//        System.out.println("e = " + e );
+
+        // write a Java program that asks the user for their age and then tells them if they are a teenager or not.
+        // bonus: if not, tell them whether they are younger or older than a teenager.
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println(" How old are you: ");
+//        int userAge = Integer.parseInt(scanner.nextLine());
+//        if (userAge > 19)
+//            System.out.println("at " + userAge + " you are older than a teenager. You are an adult.");
+//        else if (userAge < 13)
+//            System.out.println("at " + userAge + " you are younger than a teenager.");
+//        else
+//            System.out.println("at " + userAge + " you are a teenager!");
+
+//        int i = 1;
+//        System.out.println("The loop hasn't started yet. The value of i is " + i);
+//        while (i < 10){
+//            System.out.println("Now I'm in the loop. i has not incremented yet and its value is " + i);
+//            i = i + 1;
+//            System.out.println("I'm still in the loop. i has incremented now and its value is " + i);
+//        }
+//        System.out.println("This is out the far end of the loop. The conditional has tested false. The value of i is what it last incremented to. See? " + i);
+
+
+
+
       }
     }
 
