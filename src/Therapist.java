10d9da5dd9acab39e08ce022bbbce992c5128eec
Joseph Congdon <jocongdon@yahoo.com>
2021-09-08 09:33:43 -0500

    morning exercise2.0

--- src/Therapist.java
+++ src/Therapist.java
@@ -11,14 +11,14 @@ public class Therapist {
         System.out.println("How are you feeling today? ");
         userReply = scanner.nextLine();
         System.out.println(userReply);
-        if(userReply == "no")
+        if(userReply.equals ("no"))
         System.out.println("Oh really? Did you have the nightmare about the margarine again? Maybe you should stop sleeping.");
-        else if(userReply == "good")
+        else if(userReply.equals("good"))
             System.out.println("I am so glad to hear that! It means you are ready for shock therapy!");
-        else if (userReply == "ok")
+        else if (userReply.equals("ok"))
             System.out.println("Only Ok? Probably need more pills. I took some this morning.... I feel .... so good....");
             else
             System.out.println("Oh dear you are going delirious! Let's get you a lobotomy");
-        }
+
     }
 }
