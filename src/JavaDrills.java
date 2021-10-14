485ea2f762cb261caf019770b85bf9796919805c
Joseph Congdon <jocongdon@yahoo.com>
2021-10-14 09:26:14 -0500

    work in progress

---
+++ src/JavaDrills.java
@@ -0,0 +1,12 @@
+public class JavaDrills {
+    // work in progress
+    public static String flipOuterCase(String word){
+        for(int i = 0; i <= word.length(); i++){
+            String firstLetter = String.valueOf(word.charAt(0));
+            String lastLetter = String.valueOf(word.charAt(0));
+
+            if()
+        }
+    }
+
+}
