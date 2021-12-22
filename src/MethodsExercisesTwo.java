10ad1fa5d1015b58ec8d3d74a4bb7d3fcde48ce1
Joseph Congdon <jocongdon@yahoo.com>
2021-12-22 17:35:59 -0600

    fixed the division remainder

--- src/MethodsExercisesTwo.java
+++ src/MethodsExercisesTwo.java
@@ -1,3 +1,4 @@
+import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class MethodsExercisesTwo {
@@ -7,28 +8,17 @@ public class MethodsExercisesTwo {
 
     public static void main(String[] args) {
     Scanner scanner = new Scanner(System.in);
+    System.out.println("enter your first number: ");
+    int a = scanner.nextInt();
+    int b = (int) (Math.random() * 101);
 
-    int a = 0;
-    int b = 0;
 
-    double addition(a, b){
-        return a + b;
-    }
+    System.out.println("Your numbers are " + a + " and " + b);
+    System.out.println(a + " plus " + b + " equals " + (a+b));
+    System.out.println(a + " minus " + b + " equals " + (a-b));
+    System.out.println(a + " times " + b + " equals " + (a*b));
+    System.out.println(a + " divided " + b + " equals " + (double) a/b);
 
-    double subtraction(a, b){
-        return a - b;
-    }
-
-    double multiply(a, b){
-        return a * b;
-    }
-
-    double divide(a, b){
-        if (b > a) return b / a;
-        return a / b;
-    }
-
-    int sumTotal = addition() + subtraction() + multiply() + divide();
 
     }
 }
