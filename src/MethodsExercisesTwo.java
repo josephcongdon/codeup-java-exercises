1d3df1ff88ce2635e0e783f27d7e2534c7eb044d
Joseph Congdon <jocongdon@yahoo.com>
2021-12-22 14:55:52 -0600

    adding a scanner for methods exercises two

--- src/MethodsExercisesTwo.java
+++ src/MethodsExercisesTwo.java
@@ -1,4 +1,28 @@
+import java.util.Scanner;
+
 public class MethodsExercisesTwo {
 
+    double Addition(int a, int b){
+        return a + b;
+    }
 
+    double Subtraction(int a, int b){
+        return a - b;
+    }
+
+    double Multiply(int a, int b){
+        return a * b;
+    }
+
+    double divide(int a, int b){
+        if (b > a) return b / a;
+        return a / b;
+    }
+
+    Scanner scanner = new Scanner(System.in);
+
+
+    public static void main(String[] args) {
+
+    }
 }
